75a3cdb722cb25fa254a61a372ece01b4879e7d4
LamiIIIa <laminoji@gmail.com>
2026-03-28 11:03:20 +0300

    start for working

---
+++ CalcForPar/src/lvl.js
@@ -0,0 +1,260 @@
+const levels = {
+  l1: {
+    points: 20,
+    techniques: {
+      E: 5,
+      D: 2,
+      C: 1,
+      B: 0,
+      A: 0,
+      S: 0,
+      Any: 0,
+    },
+  },
+  l2: {
+    points: 24,
+    techniques: {
+      E: 5,
+      D: 3,
+      C: 1,
+      B: 0,
+      A: 0,
+      S: 0,
+      Any: 0,
+    },
+  },
+
+  l3: {
+    points: 28,
+    techniques: {
+      E: 5,
+      D: 4,
+      C: 1,
+      B: 0,
+      A: 0,
+      S: 0,
+      Any: 0,
+    },
+  },
+
+  l4: {
+    points: 32,
+    techniques: {
+      E: 5,
+      D: 4,
+      C: 2,
+      B: 0,
+      A: 0,
+      S: 0,
+      Any: 0,
+    },
+  },
+
+  l5: {
+    points: 36,
+    techniques: {
+      E: 5,
+      D: 5,
+      C: 2,
+      B: 1,
+      A: 0,
+      S: 0,
+      Any: 0,
+    },
+  },
+
+  l6: {
+    points: 40,
+    techniques: {
+      E: 5,
+      D: 5,
+      C: 3,
+      B: 1,
+      A: 0,
+      S: 0,
+      Any: 0,
+    },
+  },
+
+  l7: {
+    points: 44,
+    techniques: {
+      E: 5,
+      D: 5,
+      C: 4,
+      B: 1,
+      A: 0,
+      S: 0,
+      Any: 0,
+    },
+  },
+
+  l8: {
+    points: 48,
+    techniques: {
+      E: 5,
+      D: 5,
+      C: 4,
+      B: 2,
+      A: 0,
+      S: 0,
+      Any: 0,
+    },
+  },
+
+  l9: {
+    points: 52,
+    techniques: {
+      E: 5,
+      D: 5,
+      C: 4,
+      B: 3,
+      A: 1,
+      S: 0,
+      Any: 0,
+    },
+  },
+
+  l10: {
+    points: 56,
+    techniques: {
+      E: 5,
+      D: 5,
+      C: 5,
+      B: 3,
+      A: 1,
+      S: 0,
+      Any: 0,
+    },
+  },
+
+  l11: {
+    points: 60,
+    techniques: {
+      E: 5,
+      D: 5,
+      C: 5,
+      B: 4,
+      A: 1,
+      S: 0,
+      Any: 0,
+    },
+  },
+
+  l12: {
+    points: 64,
+    techniques: {
+      E: 5,
+      D: 5,
+      C: 5,
+      B: 4,
+      A: 2,
+      S: 0,
+      Any: 0,
+    },
+  },
+
+  l13: {
+    points: 67,
+    techniques: {
+      E: 5,
+      D: 5,
+      C: 5,
+      B: 4,
+      A: 3,
+      S: 1,
+      Any: 0,
+    },
+  },
+
+  l14: {
+    points: 70,
+    techniques: {
+      E: 5,
+      D: 5,
+      C: 5,
+      B: 5,
+      A: 3,
+      S: 1,
+      Any: 0,
+    },
+  },
+
+  l15: {
+    points: 73,
+    techniques: {
+      E: 5,
+      D: 5,
+      C: 5,
+      B: 5,
+      A: 4,
+      S: 1,
+      Any: 0,
+    },
+  },
+
+  l16: {
+    points: 76,
+    techniques: {
+      E: 5,
+      D: 5,
+      C: 5,
+      B: 5,
+      A: 4,
+      S: 2,
+      Any: 0,
+    },
+  },
+
+  l17: {
+    points: 78,
+    techniques: {
+      E: 5,
+      D: 5,
+      C: 5,
+      B: 5,
+      A: 5,
+      S: 3,
+      Any: 0,
+    },
+  },
+
+  l18: {
+    points: 80,
+    techniques: {
+      E: 5,
+      D: 5,
+      C: 5,
+      B: 5,
+      A: 5,
+      S: 3,
+      Any: 1,
+    },
+  },
+
+  l19: {
+    points: 82,
+    techniques: {
+      E: 5,
+      D: 5,
+      C: 5,
+      B: 5,
+      A: 5,
+      S: 3,
+      Any: 2,
+    },
+  },
+
+  l20: {
+    points: 84,
+    techniques: {
+      E: 5,
+      D: 5,
+      C: 5,
+      B: 5,
+      A: 5,
+      S: 3,
+      Any: 3,
+    },
+  },
+};
---
+++ CalcForPar/src/object.js
@@ -0,0 +1,33 @@
+const allcalc = document.getElementById("allcalc");
+
+const firstSection = allcalc.querySelector("#firstp");
+
+const lvlInput = allcalc.querySelector("#lvl");
+const pointsInput = allcalc.querySelector("#points");
+const firstButton = allcalc.querySelector("#firstpb");
+
+const secondSection = allcalc.querySelector("#secp");
+
+const youHavePoints = allcalc.querySelector("#yhave");
+
+const inputChakra = allcalc.querySelector("#chakra");
+const inputStrength = allcalc.querySelector("#strength");
+const inputSpeed = allcalc.querySelector("#speed");
+const inputTactics = allcalc.querySelector("#tactics");
+const inputNin = allcalc.querySelector("#nin");
+const inputTai = allcalc.querySelector("#tai");
+const inputGen = allcalc.querySelector("#gen");
+const inputFuin = allcalc.querySelector("#fuin");
+const inputHealth = allcalc.querySelector("#health");
+const inputKnowledge = allcalc.querySelector("#knowledge");
+const inputArmDef = allcalc.querySelector("#armDef");
+const inputWeapDam = allcalc.querySelector("#weapDam");
+
+const youHavePointsLeft = allcalc.querySelector("#rest");
+
+const techniquesE = allcalc.querySelector("#rE");
+const techniquesD = allcalc.querySelector("#rD");
+const techniquesC = allcalc.querySelector("#rC");
+const techniquesB = allcalc.querySelector("#rB");
+const techniquesA = allcalc.querySelector("#rA");
+const techniquesS = allcalc.querySelector("#rS");
